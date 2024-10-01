7c34ad0b4e1ebbd286227b22d9b51bf737a49397
Kian <S00237055@atu.ie>
2024-10-01 15:12:29 +0100

    task 3

--- src/routes/users.ts
+++ src/routes/users.ts
@@ -14,7 +14,7 @@ const router: Router = express.Router();
 
 router.get('/', getUsers);
 router.get('/:id', getUserById);
-router.post('/', authenticateKey, createUser);
+router.post('/', createUser);
 router.put('/:id', updateUser);
 router.delete('/:id', deleteUser);
 
