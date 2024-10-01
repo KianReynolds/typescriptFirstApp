45a70b5cf780ef57865bad0765ae42a620fc3636
Kian <S00237055@atu.ie>
2024-10-01 15:20:43 +0100

    task 6

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -4,16 +4,34 @@ import User from '../models/user'
 import { ObjectId} from 'mongodb';
 
 
-export const getUsers = (req: Request, res: Response) => {
-    //to do: get all users from the database
-  res.json({"message": "getUsers received"})
+export const getUsers =async  (req: Request, res: Response) => {
+   
+  try {
+   const users = (await usersCollection.find({}).toArray()) as User[];
+   res.status(200).json(users);
+
+ } catch (error) {
+   res.status(500).send("oppss");
+ }
 };
 
-export const getUserById = (req: Request, res: Response) => {
-  // get a single  user by ID from the database
+
+export const getUserById = async (req: Request, res: Response) => {
+  //get a single  user by ID from the database
+  
   let id:string = req.params.id;
-  res.json({"message": `get a user ${id} received`})
+  try {
+    const query = { _id: new ObjectId(id) };
+    const user = (await usersCollection.findOne(query)) as User;
+
+    if (user) {
+        res.status(200).send(user);
+    }
+} catch (error) {
+    res.status(404).send(`Unable to find matching document with id: ${req.params.id}`);
+}
 };
+
 
 export const createUser = async (req: Request, res: Response) => {
   // create a new user in the database
@@ -42,8 +60,22 @@ export const updateUser = (req: Request, res: Response) => {
   res.json({"message": `update user ${req.params.id} with data from the post message`})
 };
 
-export const deleteUser = (req: Request, res: Response) => {
-  // logic to delete user by ID from the database
+export const deleteUser = async (req: Request, res: Response) => { 
+  
+  let id:string = req.params.id;
+  try {
+    const query = { _id: new ObjectId(id) };
+    const result = await usersCollection.deleteOne(query);
 
-  res.json({"message": `delete user ${req.params.id} from the database`})
-};
+    if (result && result.deletedCount) {
+        res.status(202).json({message :`Successfully removed user with id ${id}`});
+    } else if (!result) {
+        res.status(400).json({message: `Failed to remove user with id ${id}`});
+    } else if (!result.deletedCount) {
+        res.status(404).json({message: `no user fround with id ${id}`});
+    }
+} catch (error) {
+    console.error(error);
+    res.status(400).send(error);
+}};
+
