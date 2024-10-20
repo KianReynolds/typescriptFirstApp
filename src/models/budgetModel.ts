44ab5e5225ee3f1188798b0fd1c12e644c5bea69
Kian <S00237055@atu.ie>
2024-10-20 18:30:05 +0100

    POST and GET works perfectly

--- src/models/budgetModel.ts
+++ src/models/budgetModel.ts
@@ -17,7 +17,6 @@ export const ValiadateBudget =  (budget:Budget) => {
 
     const transactionSchema = Joi.object({
         type: Joi.string().valid('income', 'expense').required(),
-        category: Joi.string().required(),
         amount: Joi.number().integer().required(),
         description: Joi.string().min(3).required(),
         date: Joi.date().default(Date.now)
@@ -25,6 +24,7 @@ export const ValiadateBudget =  (budget:Budget) => {
     })
 
     const budgetScema = Joi.object({
+        category: Joi.string().required(),
         name: Joi.string().min(3).required(),
         budgetLimit: Joi.number().integer().required(),
         transactions: Joi.array().items(transactionSchema).required()
