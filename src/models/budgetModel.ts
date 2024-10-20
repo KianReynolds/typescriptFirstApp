1d81eebc46a4e48f20704963b8190bca9094ed1e
Kian <S00237055@atu.ie>
2024-10-20 18:04:46 +0100

    all budget info(more changes required)

--- src/models/budgetModel.ts
+++ src/models/budgetModel.ts
@@ -4,10 +4,12 @@ import Joi from "joi";
 export default interface Budget{
     name: string;
     id? : ObjectId;
+    budgetLimit: number;
     category : string;
     amount: number;
     description : string;
     date : Date;
+    type: string;
 
 }
 
@@ -15,11 +17,16 @@ export const ValiadateBudget =  (budget:Budget) => {
 
     const budgetScema = Joi.object<Budget>({
         name: Joi.string().min(3).required(),
+        budgetLimit: Joi.number().integer().required(),
+
+
+        type: Joi.string().valid('income', 'expense').required(),
+
         //id: Joi.object({$oid: Joi.string()}).required(),
         category: Joi.string().required(),
         amount: Joi.number().integer().required(),
-        description: Joi.string(),
-        date: Joi.number().default(Date.now)
+        description: Joi.string().min(3).required(),
+        date: Joi.date().default(Date.now)
     })
     return budgetScema.validate(budget);
 }
