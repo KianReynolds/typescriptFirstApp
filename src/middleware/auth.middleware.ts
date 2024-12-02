412a2bca4a9699cc4f0345a4ce3bbdb40b898b30
Kian <S00237055@atu.ie>
2024-12-02 15:19:33 +0000

    task 5

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,10 +1,41 @@
-import { Request, Response, NextFunction } from 'express';
+import { Request, Response, NextFunction } from "express";
+import { verify as jwtVerify } from 'jsonwebtoken'
 
-export const authenticateKey = async (req : Request, res : Response, next : NextFunction) => {
-    const apiKey = req.headers['x-api-key'];
+export const validJWTProvided = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  
 
-    if (!apiKey) {
-        return res.status(401).json({'message' : 'Unauthorized: API key is missing'});
-    }
-    next();
-};
+      const authHeader = req.headers?.authorization;
+
+      if (!authHeader || !authHeader?.startsWith('Bearer')) {
+        console.log('no header ' + authHeader)
+            res.status(401).send();
+            return;
+      }
+
+     
+      const token: string | undefined = authHeader.split(' ')[1];
+
+      if (!token) { 
+        res.status(401).send();
+        return;
+      }
+      const secret = process.env.JWTSECRET || "not very secret";
+    
+
+      try{
+        console.log(token);
+        const payload = jwtVerify(token, secret);
+        res.locals.payload = payload;
+        next();
+            
+
+        } catch (err) {
+           res.status(403).send();
+           return;
+        }
+    };
+
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,6 +1,6 @@
 import express, {Router} from 'express';
 
-import { authenticateKey } from '../middleware/auth.middleware';
+//import { authenticateKey } from '../middleware/auth.middleware';
 import {
   getUsers,
   getUserById,
@@ -9,6 +9,7 @@ import {
   deleteUser,
   
 } from '../controllers/users';
+import { validJWTProvided } from '../middleware/auth.middleware';
 
 const router: Router = express.Router();
 
@@ -16,6 +17,6 @@ router.get('/', getUsers);
 router.get('/:id', getUserById);
 router.post('/', createUser);
 router.put('/:id', updateUser);
-router.delete('/:id', deleteUser);
+router.delete('/:id', validJWTProvided, deleteUser);
 
 export default router;
